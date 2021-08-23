a12e01a712ea1cb9dfd682696def1c35f593b79e
kaizen-ci <info@kaizen-ci.org>
2021-08-23 15:01:03 +0000

    Merge pull request #111 from atInisiatifZakat/php80
    Add support PHP 8.0

--- config/user.php
+++ config/user.php
@@ -4,10 +4,6 @@ return [
     'table_names' => [
         'users' => env('USER_TABLE_NAME_USERS', 'users'),
 
-        'auth_tokens' => env('USER_TABLE_NAME_AUTH_TOKENS', 'auth_tokens'),
-
-        'auth_token_blacklists' => env('USER_TABLE_NAME_AUTH_TOKEN_BLACKLISTS', 'auth_token_blacklists'),
-
         'personal_access_tokens' => env('USER_TABLE_NAME_PERSONAL_ACCESS_TOKENS', 'personal_access_tokens')
     ],
 ];
--- src/Providers/UserServiceProvider.php
+++ src/Providers/UserServiceProvider.php
@@ -6,18 +6,9 @@ namespace Inisiatif\Package\User\Providers;
 
 use Laravel\Sanctum\Sanctum;
 use Illuminate\Support\ServiceProvider;
-use Inisiatif\Package\User\Models\AuthToken;
-use Inisiatif\Package\User\Models\AuthTokenBlacklist;
 use Inisiatif\Package\User\Models\PersonalAccessToken;
 use Inisiatif\Package\User\Repositories\UserRepository;
-use Inisiatif\Package\User\Commands\CreateLongTokenCommand;
-use Inisiatif\Package\User\Repositories\AuthTokenRepository;
-use Inisiatif\Package\Contract\User\Model\AuthTokenInterface;
-use Inisiatif\Package\User\Repositories\AuthTokenBlacklistRepository;
-use Inisiatif\Package\Contract\User\Model\AuthTokenBlacklistInterface;
 use Inisiatif\Package\Contract\User\Repository\UserRepositoryInterface;
-use Inisiatif\Package\Contract\User\Repository\AuthTokenRepositoryInterface;
-use Inisiatif\Package\Contract\User\Repository\AuthTokenBlacklistRepositoryInterface;
 
 final class UserServiceProvider extends ServiceProvider
 {
@@ -32,12 +23,6 @@ final class UserServiceProvider extends ServiceProvider
         }
 
         $this->mergeConfigFrom(__DIR__ . '/../../config/user.php', 'user');
-
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                CreateLongTokenCommand::class,
-            ]);
-        }
     }
 
     public function register(): void
@@ -45,9 +30,5 @@ final class UserServiceProvider extends ServiceProvider
         Sanctum::ignoreMigrations();
 
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
-        $this->app->bind(AuthTokenInterface::class, AuthToken::class);
-        $this->app->bind(AuthTokenRepositoryInterface::class, AuthTokenRepository::class);
-        $this->app->bind(AuthTokenBlacklistInterface::class, AuthTokenBlacklist::class);
-        $this->app->bind(AuthTokenBlacklistRepositoryInterface::class, AuthTokenBlacklistRepository::class);
     }
 }
