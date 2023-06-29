788371aa57d3a3a80287de3d5317e098a4e93760
nuradiyana <nuradiyana@users.noreply.github.com>
2023-06-29 05:46:26 +0000

    Fix styling

--- src/Http/Resources/TokenResource.php
+++ src/Http/Resources/TokenResource.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Inisiatif\Package\User\Http\Resources;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 final class TokenResource extends JsonResource
