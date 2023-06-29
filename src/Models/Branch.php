e0c71173970c197a7f4df0959b4e4b9706216e0d
Nuradiyana <me@nooradiana.com>
2023-06-29 16:59:32 +0700

    Fixing wrong id

--- src/Models/Branch.php
+++ src/Models/Branch.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace Inisiatif\Package\User\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 final class Branch extends Model
 {
-    use HasUlids;
+    use HasUuids;
 
     protected $casts = [
         'is_head_office' => 'boolean',
