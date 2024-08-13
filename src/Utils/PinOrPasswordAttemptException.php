0892006133e660d3bb42f35391af67805425f649
Nuradiyana <me@nooradiana.com>
2024-08-13 11:43:02 +0700

    Fixing format

--- src/Utils/PinOrPasswordAttemptException.php
+++ src/Utils/PinOrPasswordAttemptException.php
@@ -8,24 +8,25 @@ use Exception;
 
 final class PinOrPasswordAttemptException extends Exception
 {
-  private string $errorType;
-  private string $customMessage;
+    private string $errorType;
 
-  public function __construct(string $errorType, string $customMessage, int $code = 422, Exception $previous = null)
-  {
-    $this->errorType = $errorType;
-    $this->customMessage = $customMessage;
-    $message = "Error Type: $errorType, Message: $customMessage";
-    parent::__construct($message, $code, $previous);
-  }
+    private string $customMessage;
 
-  public function getErrorType(): string
-  {
-    return $this->errorType;
-  }
+    public function __construct(string $errorType, string $customMessage, int $code = 422, ?Exception $previous = null)
+    {
+        $this->errorType = $errorType;
+        $this->customMessage = $customMessage;
+        $message = "Error Type: $errorType, Message: $customMessage";
+        parent::__construct($message, $code, $previous);
+    }
 
-  public function getErrorMessage(): string
-  {
-    return $this->customMessage;
-  }
+    public function getErrorType(): string
+    {
+        return $this->errorType;
+    }
+
+    public function getErrorMessage(): string
+    {
+        return $this->customMessage;
+    }
 }
